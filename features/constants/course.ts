838982e1a2eb29a2b84ac12ab399822a78f030e4
Jefferson Guirra <jeffersonguirra8@gmail.com>
2024-01-10 22:00:43 -0300

    feat: create course data to use HomePage component

---
+++ features/constants/course.ts
@@ -0,0 +1,7 @@
+import { SelectOptions } from '@/components/form/select/SelectElement'
+
+export const dataCoursers: Array<SelectOptions> = [
+    { name: 'Escolha o curso', value: 'Escolha o curso'},
+    {name: 'Robotica', value: 'robotica'},
+    { name: 'Analista de tecnologia', value: 'analista_de_tecnologia'}
+]

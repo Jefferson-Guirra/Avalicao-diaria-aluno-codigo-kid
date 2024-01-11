e12f4ffd5ac49cfec56e8a4447c3b07ea1afe0fc
Jefferson Guirra <jeffersonguirra8@gmail.com>
2024-01-10 21:59:47 -0300

    feat: create behavior data to use HomePage component

---
+++ features/constants/behavior.ts
@@ -0,0 +1,9 @@
+import { SelectOptions } from '@/components/form/select/SelectElement';
+
+export const behavior: Array<SelectOptions> =[
+  { name: 'Escolha o comportamento', value: 'Esolha o comportamento'},
+  { name: 'bom', value:'bom'},
+  { name: 'intermediário', value:'intermediário'},
+  { name: 'ruim', value:'ruim'}
+
+]

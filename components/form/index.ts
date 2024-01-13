0f0ccf8485b7588ee7d0db42223de0c9a362b7ff
Jefferson Guirra <jeffersonguirra8@gmail.com>
2024-01-12 21:59:24 -0300

    feat: create ButtonDefault component

--- components/form/index.ts
+++ components/form/index.ts
@@ -2,12 +2,14 @@ import { InputDefault } from './input/default/InputDefault'
 import { FormRoot as Root } from './root/FormRoot'
 import { SelectElement } from './select/SelectElement'
 import { DefaultTextArea as TextArea } from './textarea/default/DefaultTextArea'
+import { BtnDefault as Button } from './btn/default/BtnDefault'
 
 
 
 
 export const Form = {
   Root,
+  Button,
   InputDefault,
   SelectElement,
   TextArea

2231d3ab30b49e8da9802f216a892f4e2fa2ee04
Jefferson Guirra <jeffersonguirra8@gmail.com>
2024-01-14 20:28:54 -0300

    feat: add field Image

--- features/home/components/pdf/PdfModel.tsx
+++ features/home/components/pdf/PdfModel.tsx
@@ -1,33 +1,62 @@
-import { StudentData } from '../../pages/HomePage'
+'use client'
+
 import styles from './styles.module.css'
 import { FieldPdf } from '../pdf-field/FieldPdf'
-import Image from 'next/image'
+import { Student } from '../../@types/student'
+import GeneratePdf from '@/components/pdf/generatePdf'
+import React from 'react'
 
-export const PdfModel = ({...data}: StudentData) => {
-  console.log(data.comportment)
+interface Props extends Student {
+  images: string[]
+}
+
+export const PdfModel = ({username, classRoom, comments, course, comportment, images, phase, behavior}: Props) => {
+  const ref =React.useRef<HTMLDivElement>(null)
+
   return (
-    <article className={ styles.container}>
+    <>
+    <GeneratePdf 
+      username={ username } 
+      classRoom={ classRoom } 
+      course= { course } 
+      phase={ phase }   
+      html={ref as React.MutableRefObject<HTMLDivElement>} 
+    />
+    <article className={ styles.container} ref={ref}>
       <div className={ styles.title}>
-        <Image src={'/images/ck-logo.webp'} alt='codigo-kid-logo' width={300} height={40}/>
+        <img src={'/images/ck-logo.webp'} alt='codigo-kid-logo'/>
         <h1>Ficha Diária</h1>
       </div>
       <div className={ styles.box}>
         <FieldPdf field='Instituição' value='Codígo kid'/>
         <FieldPdf field='UF' value='Bahia' />
-        <FieldPdf field='unidade' value='Campo Formoso' />
+        <FieldPdf field='Unidade' value='Campo Formoso' />
         <FieldPdf field='Telefone' value='(74)12345-6789' />
       </div>
       <div className={ styles.box }>
-        <FieldPdf field='Aluno' value={ data.username}/>
-        <FieldPdf field='Curso' value={ data.course } />
-        <FieldPdf field='Fase' value={ data.phase }/>
-        <FieldPdf field='Aula' value={ data.classRoom } />
+        <FieldPdf field='Aluno' value={ username}/>
+        <FieldPdf field='Curso' value={ course } />
+        <FieldPdf field='Fase' value={ phase }/>
+        <FieldPdf field='Aula' value={ classRoom } />
       </div>
       <div className={ styles.box }>
-        <FieldPdf field='Comportamento' value={ data.behavior }/>
-        <FieldPdf field='Observações' value={ data.comments }/>
-        {data.comportment && <FieldPdf field='justificação do comportamento' value={ data.comportment } /> }
+        <FieldPdf field='Comportamento' value={ behavior }/>
+        {comportment && <FieldPdf field='justificação do comportamento' value={ comportment } /> }
+        <FieldPdf field='Observações' value={ comments }/>
       </div>
+      {!!images.length && (
+      <div className={ styles['box-images']}>
+        <p>Imagens:</p>
+        <div className={ styles.images }>
+          {!!images.length && images.map(url => (
+              <img key={url} src={url} alt="any_title" />
+          ))}
+        </div>
+
+      </div>
+      )}
     </article>
+
+    </>
   )
 }

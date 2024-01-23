ad72d0f47f86a20e719a66fcf6e4da898b0bf4b6
Jefferson Guirra <jeffersonguirra8@gmail.com>
2024-01-22 22:12:04 -0300

    feat: create PrepareDocument function to tranform element in pdf document

--- features/home/components/pdf/PdfModel.tsx
+++ features/home/components/pdf/PdfModel.tsx
@@ -1,30 +1,52 @@
 'use client'
-
 import styles from './styles.module.css'
 import { FieldPdf } from '../pdf-field/FieldPdf'
 import { Student } from '../../@types/student'
-import GeneratePdf from '@/components/pdf/generatePdf'
-import React from 'react'
+import React, { useRef } from 'react'
 import { contentPhases } from '@/features/constants/content'
+import jsPDF from 'jspdf'
 
 interface Props extends Student {
   images: string[]
 }
 
-export const PdfModel = ({username, classRoom, comments, course, comportment, images, phase, behavior}: Props) => {
-  const ref =React.useRef<HTMLDivElement>(null)
+export const PdfModel = ({username, classRoom, comments, course, evaluations, images, phase, behavior}: Props) => {
   const content: any = contentPhases
+  const cardRef = useRef<HTMLDivElement>(null);
+
+
+
+  const prepareDocument = async () => {
+    const cardElement = cardRef.current;
+    const fileName = username + '-' + course + '-' + phase + '-' + classRoom
+    if (!cardElement) return;
+
+    try {
+      // lazy load this package
+      const html2canvas = await import(
+        /* webpackPrefetch: true */ "html2canvas"
+      );
+
+      const result = await html2canvas.default(cardElement);
+
+      const doc = new jsPDF();
+      doc.addImage(result,'JPEG',5,22,200,160);
+      doc.save(fileName);
+    } catch (reason) {
+      console.log(reason);
+    }
+  };
+
 
   return (
     <>
-    <GeneratePdf 
-      username={ username } 
-      classRoom={ classRoom } 
-      course= { course } 
-      phase={ phase }   
-      html={ref as React.MutableRefObject<HTMLDivElement>} 
-    />
-    <article className={ styles.container} ref={ref}>
+    <div className= { styles['button-container']}>
+      <button className={ styles.btn } onClick={prepareDocument}>
+        Gerar Pdf
+      </button>
+    </div>
+
+    <div className={ styles.container} ref={cardRef}>
       <div className={ styles.title}>
         <img src={'/images/ck-logo.webp'} alt='codigo-kid-logo'/>
         <h1>Ficha Diária</h1>
@@ -33,7 +55,7 @@ export const PdfModel = ({username, classRoom, comments, course, comportment, im
         <FieldPdf field='Instituição' value='Codígo kid'/>
         <FieldPdf field='UF' value='Bahia' />
         <FieldPdf field='Unidade' value='Campo Formoso' />
-        <FieldPdf field='Telefone' value='(74)12345-6789' />
+        <FieldPdf field='Telefone' value='(74)8129-7562' />
       </div>
       <div className={ styles.box }>
         <FieldPdf field='Aluno' value={ username}/>
@@ -42,24 +64,23 @@ export const PdfModel = ({username, classRoom, comments, course, comportment, im
         <FieldPdf field='Aula' value={ classRoom } />
       </div>
       <div className={ styles.box }>
-        <FieldPdf field=' Conteúdo' value={ content[course][phase][classRoom] }/>
+        <FieldPdf field='Avaliação' value={ evaluations }/>
+        <FieldPdf field='Conteúdo' value={ content[course][phase][classRoom] }/>
         <FieldPdf field='Comportamento' value={ behavior }/>
-        {comportment && <FieldPdf field='justificação do comportamento' value={ comportment } /> }
         <FieldPdf field='Observações' value={ comments }/>
       </div>
       {!!images.length && (
       <div className={ styles['box-images']}>
         <p>Imagens:</p>
         <div className={ styles.images }>
-          {!!images.length && images.map(url => (
-              <img key={url} src={url} alt="any_title" />
+          {!!images.length && images.map((url, index) => (
+              <img id='image' key={index} src={url} alt="imagem-projeto"  width="300" height="200" />
           ))}
         </div>
 
       </div>
       )}
-    </article>
-
+    </div>
     </>
   )
 }
